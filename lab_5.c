8ab02c6ba4bcd8051501a9c084f576074bdf87a4
KovalchukM <m.kovalchuk@g.nsu.ru>
2016-12-15 01:22:22 +0700

    Update lab_5.c

--- lab_5.c
+++ lab_5.c
@@ -3,45 +3,56 @@
 
 #define SIZE 64
 
-int to_Base(char *Library)
+int to_Base(char *Library, char *input)
 {
-	char elem[3];
-	char base_elem[4];
-	FILE *base64 = fopen ( "tobase.txt", "r");
+	char elem[3] = {0};
+	char base_elem[4] = {0};
+	FILE *base64 = fopen ( input, "r");
 	FILE *out = fopen ( "out.base64.txt", "w+");
+	  if (base64 == NULL) 
+	  {
+	  	printf("open error.\n");
+	  	return(1);
+	  }
 	while (1)
 	{
-		for (int i = 0; i < 3; i++)
-			{
-				elem[i] = fgetc(base64);
-			}
-		printf("elem =%c  %c   %c\n",elem[0], elem[1], elem[2] );
-		if (elem[0] == EOF)
-			break;
-		base_elem[0] = elem[0] >> 2;
-		if (elem[1] == EOF)
+		printf("%s\n","begin" );
+		int end_check = fread(elem, sizeof(char) , 3, base64);
+		if (end_check == 0)
 		{
-			base_elem[1] = ((elem[0] << 4)& 0x3F);
-			fprintf(out, "%c",Library[base_elem[0]] );
-			fprintf(out, "%c",Library[base_elem[1]] );
-			fprintf(out, "%s","==" );
 			break;
 		}
-		base_elem[1] = (((elem[0] << 4) | (elem[1] >> 4)) & 0x3F);
-		if (elem[2] == EOF)
+		if (end_check == 1)
 		{
+			base_elem[0] = elem[0] >> 2;
+			base_elem[1] = ((elem[0] << 4)& 0x3F);
+			fprintf(out, "%c", Library[base_elem[0]] );
+			fprintf(out, "%c", Library[base_elem[1]] );
+			fprintf(out, "%s", "==" );
+			break;
+		}
+		if (end_check == 2)
+		{
+			base_elem[0] = elem[0] >> 2;
+			base_elem[1] = (((elem[0] << 4) | (elem[1] >> 4)) & 0x3F);
 			base_elem[2] = ((elem[1] << 2) & 0x3F);
 			for (int i = 0; i < 3; i++)
 				fprintf(out, "%c",Library[base_elem[i]] );
 			fprintf(out, "%c", '=' );
 			break;
 		}
-		base_elem[2] = (((elem[1] << 2) | (elem[2] >> 6)) & 0x3F);
-		base_elem[3] = (elem[2] & 0x3F);
-		printf("base_elem =%d    %d     %d    %d\n",base_elem[0],base_elem[1],base_elem[2], base_elem[3] );
-		for (int i = 0 ; i < 4 ; i++)
+		if (end_check == 3)
 		{
-			fprintf(out, "%c", Library[base_elem[i]] );
+			base_elem[0] = elem[0] >> 2;
+			base_elem[1] = (((elem[0] << 4) | (elem[1] >> 4)) & 0x3F);
+			base_elem[2] = (((elem[1] << 2) | (elem[2] >> 6)) & 0x3F);
+			base_elem[3] = (elem[2] & 0x3F);
+			for (int i = 0 ; i < 4 ; i++)
+			{
+				printf("%c", base_elem[i] );
+				fprintf(out, "%c", Library[base_elem[i]] );
+			}
+			printf("\n");
 		}
 	}
 	printf("%s\n","coding complete" );
@@ -49,27 +60,30 @@ int to_Base(char *Library)
 	fclose (out);
 }
 
-int From_Base( char *Library)
+int From_Base(char *Library, char *input)
 {
-	char base_elem[4];
+	char base_elem[4] = {0};
 	int base_elem_index[4];
 	char elem[3];
-	FILE *base64;
-	FILE *out;
-	base64 = fopen("out.base64.txt", "r");
-	out = fopen("out.base64.orig.txt", "w+");
+	FILE *base64 = fopen(input, "r");
+	FILE *out = fopen("out.base64.orig.txt", "w+");
+	  if (base64 == NULL) 
+	  {
+	  	printf("open error\n");
+	  	return(1);
+	  }
 	while(1)
 	{
+		int end_check = fread(base_elem, sizeof(char) , 4, base64);
 		for (int i = 0; i < 4; i++)
 		{
-			base_elem[i] = fgetc(base64);
 			for(int k = 0; k < SIZE; k++)
 			{
 				if (base_elem[i] == Library[k])
 					base_elem_index[i] = k;
 			}
 		}
-		if (base_elem[0] == EOF)
+		if (end_check == 0)
 			break;
 		if (base_elem[1] == '=')
 		{
@@ -102,15 +116,15 @@ int From_Base( char *Library)
 	fclose(out);
 }
 
-int main ()
+int main (int argc , char *argv[])
 {
 	char Library[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	printf("%s\n","press 1 to coding" );
 	printf("%s\n", "press 2 to decoding" );
-	int n;
+	int n = 0;
 	scanf ("%d",&n);
 	if (n == 1)
-		to_Base (Library);
+		to_Base (Library, argv[1]);
 	if (n == 2)
-		From_Base (Library);
+		From_Base (Library, argv[1]);
 }
